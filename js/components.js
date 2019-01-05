be68f50f4edfd29e76befdd22ef02f2a9ac82e5f
yue.lab <gitlab@rainy.me>
2019-01-06 08:30:36 +0900

    init

---
+++ js/components.js
@@ -0,0 +1,11 @@
+customElements.define('city-container',
+    class extends HTMLElement {})
+
+customElements.define('city-block',
+    class extends HTMLElement {})
+
+customElements.define('city-name',
+    class extends HTMLElement {})
+
+customElements.define('city-intro',
+    class extends HTMLElement {})
